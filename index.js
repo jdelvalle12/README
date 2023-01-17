0077b3a307b5f9261b56c2b5fa0b30c32f4c27cd
Jose Del Valle <jdelvalle88@live.com>
2023-01-17 18:34:50 -0500

    little bit of codes in JS

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    this.fileName = fileName;
+    this.data = data;
+    this.
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
